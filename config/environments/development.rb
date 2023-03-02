f45350a2c217185ba654352e1ef2be0c42b5e8f6
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-03-02 17:40:47 +0200

    Remove confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -52,6 +51,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
