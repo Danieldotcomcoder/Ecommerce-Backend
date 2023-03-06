7db387d79e71ceb041958549ff50eb1019706d7b
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-03-06 19:35:55 +0200

    add defaults and migrate

--- db/migrate/20230306172503_create_items.rb
+++ db/migrate/20230306172503_create_items.rb
@@ -1,14 +1,13 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.integer :id
       t.string :name
       t.string :image
       t.text :description
-      t.string :color
-      t.string :size
+      t.string :color, array: true, default: ['black','grey','white']
+      t.string :size, array: true, default: ['XS','S','M','L','XL','XXL']
       t.integer :price
-      t.integer :count
+      t.integer :count, default: 1
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_103154) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_172503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.text "description"
+    t.string "color", default: ["black", "grey", "white"], array: true
+    t.string "size", default: ["XS", "S", "M", "L", "XL", "XXL"], array: true
+    t.integer "price"
+    t.integer "count", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "firstName"
